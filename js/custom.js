6d517815055916965a70880dd428832342f6fc6b
DXEL Network <ask@dxel.net>
2024-02-28 16:25:57 +0600

    Delete js directory

--- js/custom.js
+++
@@ -1,8 +0,0 @@
-// display current year on footer
-function getCurrentYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
-
-getCurrentYear();
