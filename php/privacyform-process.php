c46405fd0f404ce61db3feaaea0642047290d6a7
DXEL Network <ask@dxel.net>
2024-07-05 16:00:34 +0600

    Update privacyform-process.php

--- php/privacyform-process.php
+++ php/privacyform-process.php
@@ -25,7 +25,7 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "dxel@outlook.com";
+$EmailTo = "inform.milon@gmail.com";
 $Subject = "New privacy request from Aria landing page";
 
 // prepare email body text
